ea8c87959373f0605b5fc7fb9eea5cfea666edc4
Hakob Muradyan <hakobmuradyan1996@gmail.com>
2020-05-13 04:40:05 -0700

    Just a cheater

---
+++ script.js
@@ -0,0 +1,31 @@
+var clickZoom = document.getElementById("prof-image");
+var zoom = document.getElementById("zoomedIn");
+var menu = document.getElementById("menu");
+var scan = document.getElementById("scan");
+var ready = document.getElementById("readyP");
+var current = document.getElementById("current");
+var zoomToScan = document.getElementById("orangePart");
+var toMenu = document.getElementById("toMain");
+
+
+clickZoom.addEventListener('click', function(){
+    zoom.style.display = "block";
+    menu.style.display = "none";
+});
+
+zoomToScan.addEventListener('click', function(){
+    menu.style.display = "block";
+    zoom.style.display = "none";
+});
+
+current.addEventListener('click', function(){
+    menu.style.display = "none";
+    ready.style.display = "block";
+});
+
+toMenu.addEventListener('click', function(){
+    menu.style.display = "block";
+    ready.style.display = "none";
+});
+
+
